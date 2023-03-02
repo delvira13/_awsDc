a796b2daf18ea2436e4e770303a18dfc7564f484
delvirasa <delvira@datacentric.es>
2023-03-02 20:15:02 +0100

    .

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='de-awsUtils',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/delvira13/_awsUtils',
     packages=['awsUtils'],
     install_requires=requires,
